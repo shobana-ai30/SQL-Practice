c2984e24cf888b193f87c8efdd53b394ee131cc2
Shobana Iyyasami <93684083+shobana-ai30@users.noreply.github.com>
2025-04-12 05:53:49 -0500

    Update 595_Big Countries.sql

--- Leet_Code/Basic_Select/595_Big Countries.sql
+++ Leet_Code/Basic_Select/595_Big Countries.sql
@@ -1,4 +1,7 @@
-"Table: World
+"Problem : 595 
+ Level : Easy
+ Question : Write a solution to find the name, population, and area of the big countries.
+ Table: World
 
 +-------------+---------+
 | Column Name | Type    |
@@ -17,7 +20,6 @@ A country is big if:
 
 it has an area of at least three million (i.e., 3000000 km2), or
 it has a population of at least twenty-five million (i.e., 25000000).
-Write a solution to find the name, population, and area of the big countries.
 
 Return the result table in any order.
 
